73a69e323f19c42bac573f8eaff168efe190156d
Alex Nitta <a.j.nitta@gmail.com>
2016-02-01 14:32:07 -0800

    Finish implementing getElementsByClassName
    Using references to children and siblings, check className
    against the list of classes on each element.
    Return array of all elements that match the className.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,27 +4,42 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function (className, nodeArr, resultArr) {
-  nodeArr = nodeArr || ( // if nodeList is undefined, set it to all nodes in body
-    (function (nodes) {
-      var arr = [];
-      for (var i = 0; i < nodes.length; i++) {
-        arr.push(nodes[i]);
-      }
-      return arr;
-    })(document.body.childNodes);
-  ); 
+var getElementsByClassName = function (className) {
   
-  resultArr = resultArr || []; // if resultArr is undefined, set it to empty array
-  
-  if (nodeArr[0].classList !== undefined && nodeArr[0].classList.item(0) !== null) {
-    for (var j = 0; j < nodeArr[0].classList.length; j++) {
-      resultArr.push(nodeArr[0].classList.item(j));
-    }
+  if (document.body === null) { // if document has no body, return null
+    return null;
   }
+  
+  var innerFunc = function (className1, node, resultArr) {  
+    node = node || document.body; // if node is undefined, set it to document body
+    resultArr = resultArr || []; // if resultArr is undefined, set it to empty array
+    
+    if (node.classList !== undefined) {  // if this node has any classes,
+      var matchClass = false;      
+      for (var k = 0; k < node.classList.length; k++) { // check all its classes against className1
+        if (className1 === node.classList[k]) {
+          matchClass = true;
+        }
+      } 
+      if (matchClass) { // if any class matches,
+        resultArr.push(node) // push node to resultArr
+      };
+    }
+    
+    if (node.hasChildNodes()) { // if this node has children
+      var child = node.firstChild; // start with the firstChild
+      while (child) { // as long as there is a child to work on,
+        if (child.nodeType === 1) { // if they are an element,
+          innerFunc(className1,child,resultArr); // recursive case: run innerFunc on the child
+        }
+        child = child.nextSibling; // when done with this child, move on to the next one
+      }
+    }
+    
+    return resultArr; // when all children have been recursed, return resultArr     
+  };
+  
+  return innerFunc(className);  // invoke inner function on supplied className
+};
 
-  nodeArr.shift();
-  
-  getElementsByClassName(className,nodeArr,resultArr);
-  
-}
+
