500ac7a2acc39838668e539be5fb80476ec2a148
Alex Nitta <a.j.nitta@gmail.com>
2016-02-01 07:40:53 -0800

    Begin work on recursion through document class names

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,11 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
-  // your code here
+var getElementsByClassName = function (className, nodeArr, resultArr) {
+  nodeArr = nodeArr || ( // if nodeList is undefined, set it to all nodes in body
+    (function (nodes) {
+      var arr = [];
+      for (var i = 0; i < nodes.length; i++) {
+        arr.push(nodes[i]);
+      }
+      return arr;
+    })(document.body.childNodes);
+  ); 
   
-  var elementList = document.querySelectorAll('.' + className);
+  resultArr = resultArr || []; // if resultArr is undefined, set it to empty array
   
-  return elementList;
+  if (nodeArr[0].classList !== undefined && nodeArr[0].classList.item(0) !== null) {
+    for (var j = 0; j < nodeArr[0].classList.length; j++) {
+      resultArr.push(nodeArr[0].classList.item(j));
+    }
+  }
+
+  nodeArr.shift();
   
-};
+  getElementsByClassName(className,nodeArr,resultArr);
+  
+}
